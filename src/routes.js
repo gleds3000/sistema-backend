114e1abe3e2dd1888832fec2b962b6975a216778
Gleds3000 <gleds3000@gmail.com>
2018-11-23 12:15:16 -0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,11 @@ const routes = express.Router();
 const TweetControllers = require('./controllers/tweetcontrollers');
 const LikeControllers = require('./controllers/likecontrollers');
 
-/*
+/* teste */
 routes.get("/", (req, res) =>{
     return res.send('hello rotas');
 });
-*/
+// fim do teste 
 routes.get("/tweets", TweetControllers.index);
 routes.post("/tweets", TweetControllers.store);
 routes.post('/likes/:id', LikeControllers.store);
